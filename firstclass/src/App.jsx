a4d861589bf459d0bb76557cacdaed85409aebf9
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-18 13:45:55 +0530

    feat: update App component

--- firstclass/src/App.jsx
+++ firstclass/src/App.jsx
@@ -1,12 +1,36 @@
+
+
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+ 
+   let [counter,setCounter] = useState(15)
+   
+  
+  const addValue = () =>{
+    setCounter(counter+1)
+    console.log("clicked", counter);
+  }
+
+
+  const removeValue = () =>{
+    setCounter(counter - 1)
+
+  }
+
 
   return (
   <>
 
     <h1>This is React</h1>
-    <h2>And This is some random Text</h2>
+    <h2>Counter the Value:{counter}</h2>
+
+
+    <button onClick={addValue} >Add Value</button>
+    <br />
+    <button onClick={removeValue}>Remove Value</button>
 
   </>
   )
