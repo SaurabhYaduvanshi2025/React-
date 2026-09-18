0067c431f187514a81479a17a087465d4649f7e7
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-18 18:20:15 +0530

    feat: Update App component

--- firstclass/src/App.jsx
+++ firstclass/src/App.jsx
@@ -1,39 +1,46 @@
 
 
-import { useState } from 'react';
-import './App.css'
+  import { useState } from 'react';
+  import './App.css'
 
-function App() {
+  function App() {
 
- 
-   let [counter,setCounter] = useState(15)
-   
   
-  const addValue = () =>{
-    setCounter(counter+1)
-    console.log("clicked", counter);
+    let [counter,setCounter] = useState(15)
+    
+    
+    const addValue = () =>{
+
+      if(counter<20){
+        setCounter(counter+1)
+      }
+      console.log("clicked", counter);
+    }
+
+
+    const removeValue = () =>{
+      if(counter > 0){
+
+        setCounter(counter - 1)
+
+      }
+
+    }
+
+
+    return (
+    <>
+
+      <h1>This is React</h1>
+      <h2>Counter the Value:{counter}</h2>
+
+
+      <button onClick={addValue} >Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+
+    </>
+    )
   }
 
-
-  const removeValue = () =>{
-    setCounter(counter - 1)
-
-  }
-
-
-  return (
-  <>
-
-    <h1>This is React</h1>
-    <h2>Counter the Value:{counter}</h2>
-
-
-    <button onClick={addValue} >Add Value</button>
-    <br />
-    <button onClick={removeValue}>Remove Value</button>
-
-  </>
-  )
-}
-
-export default App
+  export default App
