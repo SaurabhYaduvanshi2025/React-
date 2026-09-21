8ffd7d0d8f333984b638321bb8805464fafc8a48
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-21 12:51:24 +0530

    feet: add second projects

---
+++ tailprops/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import React from 'react'
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+    <>
+       <h1 className='bg-green-400 text-black rounded-xl m-100 p-3.5' >This is practice of tailwind css with React props</h1>
+        <Hero/>
+
+    </>
+  )
+}
+
+export default App
