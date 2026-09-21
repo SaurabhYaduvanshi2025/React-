c94907ac7c25c84c859141d5baa66712b50190f0
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-21 13:21:47 +0530

    changes in components and functions

--- tailprops/src/App.jsx
+++ tailprops/src/App.jsx
@@ -1,13 +1,41 @@
 import './App.css'
 // import React from 'react'
-import Hero from './components/Hero'
+import ProfileCard from './components/ProfileCard'
 
 function App() {
 
   return (
     <>
-       <h1 className='bg-green-400 text-black rounded-xl m-100 p-3.5' >This is practice of tailwind css with React props</h1>
-        <Hero/>
+       
+          <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Team Members</h1>
+
+      {/* 2. Component ko alag-alag data ke sath call kiya */}
+      <div className="flex flex-wrap">
+        <ProfileCard 
+          name="Aman Sharma" 
+          role="Frontend Developer" 
+          experience="2 Years" 
+        />
+
+        <ProfileCard 
+          name="Rohit Verma" 
+          role="UI/UX Designer" 
+          experience="1 Year" 
+        />
+
+        <ProfileCard 
+          name="Pooja Singh" 
+          role="Backend Developer" 
+          experience="3 Years" 
+        />
+        <ProfileCard
+         name="Saurabh Yadav"
+         role="Fullstack"
+         experience="1 Years"
+         />
+      </div>
+    </div>
 
     </>
   )
